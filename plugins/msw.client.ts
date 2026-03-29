7ca98ab0af720db2ed345a09bc967e80c8419479
Tolmas001 <urinovtolmas20@gmail.com>
2026-03-30 00:57:32 +0500

    cdf

--- plugins/msw.client.ts
+++ plugins/msw.client.ts
@@ -1,6 +1,5 @@
 export default defineNuxtPlugin(async () => {
   if (process.server) return
-  if (import.meta.env.MODE !== 'development' && import.meta.env.PROD) return
 
   const { worker } = await import('~/browser')
   await worker.start({
