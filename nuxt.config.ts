e5afeb6206731c694787d6b3252487a5a79d99e5
Tolmas001 <urinovtolmas20@gmail.com>
2026-03-30 01:26:08 +0500

    feat: add favicon to public directory and configure in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
         { name: 'description', content: 'Мини-приложение для управления задачами с авторизацией' },
       ],
       link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
